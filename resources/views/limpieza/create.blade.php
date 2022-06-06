054270df3ae722e4ad5ddda4e1f8e038d80b4c96
rafapalos <rafaelpalosruiz@gmail.com>
2022-06-06 09:10:49 +0200

    matricula required

--- resources/views/limpieza/create.blade.php
+++ resources/views/limpieza/create.blade.php
@@ -15,15 +15,16 @@
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Matricula</label>
-            <input id="matricula" name="matricula" type="text" class="form-control" required>
+            <input id="matricula" name="matricula" type="text" maxLength="8" pattern="[0-9]{4}[-][A-Z]{3}" class="form-control" required>
+            <p>La matricula debe tener el siguiente formato "0000-XXX"</p>
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Marca</label>
-            <input id="marca" name="marca" type="text" class="form-control" required>
+            <input id="marca" name="marca" type="text" maxLength="15" pattern="[A-Za-z ]{1,15}" class="form-control" required>
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Modelo</label>
-            <input id="modelo" name="modelo" type="text" class="form-control" required>
+            <input id="modelo" name="modelo" type="text" maxLength="15" pattern="[A-Za-z0-9]{1,15}" class="form-control" required>
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Tipo de Lavado</label>
